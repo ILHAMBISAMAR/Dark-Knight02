f2dbab3b00f6618cf453cecdc326cafb456c567c
ILHAMBISAMAR <73697931+ILHAMBISAMAR@users.noreply.github.com>
2020-11-21 22:49:40 +0700

    Add files via upload

---
+++ handler/message/text/id.js
@@ -0,0 +1,302 @@
+exports.textTnC = () => {
+    return `
+🌷🌷🌷🌷🌷🌷
+Source code / bot ini merupakan program open-source (N3IB) yang ditulis menggunakan Javascript, kamu boleh follow ig ilham.bsmr kok.
+🍀🍀🍀🍀🍀🍀
+Dengan menggunakan source code / bot ini maka anda setuju dengan Syarat dan Kondisi sebagai berikut:
+🍁🍁🍁🍁🍁🍁
+⚜ Source code / bot tidak menyimpan
+  data anda di server kami paham 
+  ngentod.
+⚜ Source code / bot tidak
+  bertanggung jawab atas sticker 
+  yang anda buat dari bot ini serta 
+  video, gambar maupun data lainnya 
+  yang anda dapatkan dari Source code 
+  / bot ini.
+⚜ Source code / bot tidak boleh 
+  digunakan untuk layanan yang 
+  bertujuan/berkontribusi dalam: 
+    🌟 seks Boleh Kok asal dah hallal
+    🌟 perjudian boleh kok kalo ada
+      uang
+    🌟 perilaku adiktif yang merugikan
+      boleh kalo yg dirugikan diri lo
+      sendiri
+    🌟 kejahatan boleh kok asal ga
+      keterlaluan
+    🌟 kekerasan dalam rumah tangga
+      ngentod jangan lupa di videoin
+    🌟 pembakaran hutan / penggundulan
+      hutan boleh kok bapak lo punya
+      hutan
+    🌟 Lah anjing kalian ga follow IG 
+      GUA BISULAN LO
+
+💧 Source Code BOT : https://github.com/IlhamBismar/Dark-Knight
+💧 NodeJS WhatsApp library: https://github.com/open-wa/wa-automate-nodejs
+
+⚜ Best regards, ilham bismar.`
+}
+
+exports.textMenu = (pushname) => {
+    return `
+Hai, ${pushname} Kenalan yuk? 👋️
+
+🗡🗡*PESAN PENTING*🗡🗡
+
+TOLONG SETELAH BOT DI INVITE KEDALAM GRUP
+TOLONG FOLLOW Instagram gua ya ‼
+
+https://instagram.com/ilham.bsmr
+
+MINIMAL FOLLOW 10 ORANG MASING" DALAM GRUP
+JIKA TIDAK BOT AKAN OUT OTOMATIS DALAM 1 HARI
+
+*_TERIMA KASIH ATAS PERHATIANYA_*
+
+☃ *SEBELEM BACA MENU ALANGKAH BAIKNYA BACA AYAT KURSI DULU BRO* 
+☃
+*بِسْمِ اللّهِ الرَّحْمَنِ الرَّحِيْمِ* 
+
+*ٱللَّهُ لَآ إِلَٰهَ إِلَّا هُوَ ٱلْحَىُّ ٱلْقَيُّومُ ۚ لَا تَأْخُذُهُۥ سِنَةٌ وَلَا نَوْمٌ ۚ لَّهُۥ مَا فِى ٱلسَّمَٰوَٰتِ وَمَا فِى ٱلْأَرْضِ ۗ مَن ذَا ٱلَّذِى يَشْفَعُ عِندَهُۥٓ إِلَّا بِإِذْنِهِۦ ۚ يَعْلَمُ مَا بَيْنَ أَيْدِيهِمْ وَمَا خَلْفَهُمْ ۖ وَلَا يُحِيطُونَ بِشَىْءٍ مِّنْ عِلْمِهِۦٓ إِلَّا بِمَا شَآءَ ۚ وَسِعَ كُرْسِيُّهُ ٱلسَّمَٰوَٰتِ وَٱلْأَرْضَ ۖ وَلَا يَـُٔودُهُۥ حِفْظُهُمَا ۚ وَهُوَ ٱلْعَلِىُّ ٱلْعَظِيمُ
+
+
+👉 Berikut adalah beberapa fitur yang ada pada bot ini!✨
+
+💞 Kata Pengantar 💞
+
+*بِسْمِ اللّهِ الرَّحْمَنِ الرَّحِيْمِ*  
+
+☢. *Peringatan/penting*
+Untuk jangan PC bot bagi yg cwok, kalo cwek boleh PC bot asal kenalan dulu.
+Yg cwok PC bot berarti lo gay anjing.
+
+❄ 01. *BasaBasi*
+
+Ga usah basa basi anjing langsung aja masuk GC Squad BARBAR. 
+
+❄ 02. *FollowIg*
+
+⚜Lah anjing cuman make bot ga follow bisulan lo anjing.
+
+✔IG :https://instagram.com/ilham.bsmr
+
+❄ 03. *KuatTahanLama*
+Jangan kebanyakan Coli lah bangsat.
+
+❄ 04. *CewekSaveWaGuaYak*
+cowok jangan PC gua anjing.
+
+❄ 05.*hallo*
+
+🏍 Sticker Creator:
+
+🎆 1. *#sticker*
+Untuk merubah gambar menjadi sticker. 
+Penggunaan: kirim gambar dengan caption #sticker atau balas gambar yang sudah dikirim dengan #sticker
+
+🎆 2. *#sticker* _<Url Gambar>_
+Untuk merubah gambar dari url menjadi sticker. 
+Penggunaan: 
+
+🎆 3. *#gifsticker* _<Giphy URL>_ / *#stickergif* _<Giphy URL>_
+Untuk merubah gif menjadi sticker (Giphy Only)
+Penggunaan: Kirim pesan dengan format *gifsticker https://media.giphy.com/media/JUvI2c1ddyzkwK4RlV/giphy.gif*
+
+🎆 4. *#memesticker* _<teks atas>_ | _<teks bawah>_
+Untuk membuat sticker meme dengan teks atas dan bawah
+Penggunaan: kirim gambar dengan caption _*#meme aku atas | kamu bawah*_, atau juga bisa dengan membalas gambar yang sudah ada.
+
+🛠 Downloader:
+
+🌸 1. *#tiktok* _<tiktok url>_
+Untuk mengunduh video dari video tiktok.
+Penggunaan: kirim pesan dengan format *#tiktok https://www.tiktok.com/@itsandani/video/6869248690381425922* 
+
+🌸 2. *#fb* _<post/video url>_
+Untuk mengunduh video dari Facebook.
+Penggunaan: kirim pesan dengan format *#fb https://www.facebook.com/.....*
+
+🌸 3. *#ig* _<instagram post url>_
+Untuk mengunduh photo dan video dari instagram.
+Penggunaan: kirim pesan dengan format *#ig https://www.instagram.com/p/BPOd1vhDMIp/*
+
+🌸 4. *#twt* _<twitter post url>_
+Untuk mengunduh photo dan video dari Twitter.
+Penggunaan: kirim pesan dengan format *#twt https://twitter.com/ntsana_/status/1306108656887324672*
+
+🌸 5. *!brainly*
+Ini kalo ga salah ga bisa anjing,ngeyel amat bangsat.
+
+⚙ Lain-lain:
+
+🔥 1. *#translate* _<kode bahasa>_
+Untuk mengartikan pesan menjadi bahasa yang ditentukan.
+Penggunaan: Balas/quote/reply pesan yang ingin kamu translate dengan _*#translate id*_ <- id adalah kode bahasa. kode bahasa dapat dilihat di *https://bit.ly/33FVldE*
+
+🔥 2. *#resi* _<kurir>_ _<nomer resi>_
+Untuk memeriksa status pengiriman barang, daftar kurir: jne, pos, tiki, wahana, jnt, rpx, sap, sicepat, pcp, jet, dse, first, ninja, lion, idl, rex.
+Penggunaan: kirim pesan dengan format _*#resi jne 1238757632*_
+
+🔥 3. *#meme* _<teks atas>_ | _<teks bawah>_
+Untuk membuat meme dengan teks atas dan bawah
+Penggunaan: kirim gambar dengan caption _*#meme aku atas | kamu bawah*_, atau juga bisa dengan membalas gambar yang sudah ada.
+
+🔥 4. *#ceklokasi*
+Cek lokasi penyebaran covid-19 di daerah sekitarmu (kelurahan).
+Penggunaan: kirimkan lokasimu lalu balas/quote/reply lokasi yang kamu kirim dengan _*#ceklokasi*_
+
+🔥 5. *#tnc*
+Menampilkan Syarat dan Kondisi Bot.
+
+🔥 6.*#donasi*
+menampilkan informasi donasi.
+
+🐱‍👤 Lah anjing ga usah basa basi gelud aja kita anak dajjal‼🤼‍`
+}
+
+exports.textAdmin = (pushname) => {
+    return `
+
+💕Hallo Admin, *${pushname}* sayang 😘😘😘
+
+⚠ [ *Admin Group Only* ] ⚠ 
+⚜Berikut adalah beberapa fitur admin grup yang ada pada bot ini‼
+
+🔥  0. *peringatan/penting*
+
+☣ 69 adalah stayle yg sering di mainkan bintang bokep.
+
+❄ 1.*#kick* @user
+Untuk mengeluarkan member dari grup (bisa lebih dari 1).
+
+❄ 2.*#promote* @user
+Untuk mempromosikan member menjadi Admin grup.
+
+❄ 3.*#demote* @user
+Untuk demosikan Admin grup.
+
+❄ 4.*#del*
+Untuk menghapus pesan bot (balas pesan bot dengan #del)
+
+❄🔥💧❄🔥🔥❄🔥💧❄🔥💧❄🔥💧`
+}
+
+exports.textDonasi = (pushname) => {
+    return `
+💕Hallo, *${pushname}* 💞
+✔Mau donasi? 😘❤😘
+
+🗡🗡*PESAN PENTING*🗡🗡
+
+TOLONG SETELAH BOT DI INVITE KEDALAM GRUP
+TOLONG FOLLOW Instagram gua ya !!
+
+https://instagram.com/ilham.bsmr
+
+MINIMAL FOLLOW 10 ORANG MASING" DALAM GRUP
+JIKA TIDAK BOT AKAN OUT OTOMATIS DALAM 1 HARI
+
+*_TERIMA KASIH ATAS PERHATIANYA_*
+
+⏰ *24-03-2000*
+⏰ *Ketika Tuhan Berkehendak*
+
+🔥 Silahkan donasi dibawah ini Ya syng:
+   
+❄ *OVO*: 085363439190
+❄ *DANA*: 088271899842
+❄ *PULSA*: 085363439190
+
+📺 *PESAN* : Cara Membawa uang saat mati Ya Donasi
+
+✅ Follow akun instagram Gua Ya https://instagram.com/ilham.bsmr
+
+🎇 INFORMASI COVID-20 KARENA UDAH 2020 COVID DI UPGRADE!
+
+🚑 POSITIF  : 684
+🚑 SEMBUH   : 537
+🚑 MENINGGAL: 437
+🚑 UPDATE   : 527
+
+🎇 _TETAP JAGA KESEHATAN DAN JANGAN SAMPAI MATI YA_
+
+🎇 Mau pasang iklan di *Dark-Knight?*
+☎️ WA : *085363439190*
+  
+🔥 Gunakan bot dengan bijak ‼️
+🔥 Anda Sopan Saya Segan ‼️
+
+💮 Ramein Grub  : https://chat.whatsapp.com/Lde0MAbjsLcKUhlEKyLla3
+
+💮 Ramein Grub  : https://chat.whatsapp.com/Jb7SwidDJKO58nOgufQ0Cp
+
+  
+🌟 *Donasi ya syang BY Dark-Knight* 🌟`
+}
+
+
+exports.textHallo = (pushname) => {
+    return `
+hallo juga, ${pushname} Bangsat !
+
+Apa ada yg bisa bot bantu?
+
+ini adalah menu spesial bot untuk mengajak bot berbicara ya..
+
+1. *#perkenalan*
+
+2. *#P*
+
+3. *#Agama*
+
+4. *#Pacar*
+
+5. *#Toxic*
+
+Teriamaksih anak-anak dajjal !!!.`
+}
+
+
+exports.textPerkenalan = (pushname) => {
+    return `
+Perkenalkan Nama Bot Adalah Dark Knight salam kenal, ${pushname} Bangsat !
+Oiya satu lagi Bot itu cowok ya!!
+Bot dijalankan sistem komputer tapi bot bisa jatuh cinta juga hehe.`
+}
+ 
+
+exports.textP = (pushname) => {
+    return `
+Apa P? kamu gak punya agama dan tata krama ya, ${pushname}.
+kalo kamu islam awali dg assalamu'alaikum wr wb ya, ${pushname}
+Kalo kamu non muslim awali dg kata-kata yg indah ya.`
+}
+ 
+
+exports.textAgama = (pushname) => {
+    return `
+Kamu nanya agama bot ya, ${pushname} ?
+Agama bot islam tapi kita harus saling menghargai ya, ${pushname}
+Bagi teman-teman yg non muslim kita harus saling menghargai ya
+Terima kasih 
+salam dari Bot Dark Knight.`
+}
+
+exports.textPacar = (pushname) => {
+    return `
+Apa Pacar?...
+Hmm pertanyaan yg sulit buat bot jawab...
+kalo kamu cewek kamu mau gak pacaran sama bot?, ${pushname} ?
+kalo kamu mau ayook kita pacaran hehe.`
+}
+
+exports.textToxic = (pushname) => {
+    return `
+Wah ini yg bot tunggu-tunggu member dan admin bangsat kamu, ${pushname} paling Bangsat !
+Dasar otak udang kalian anjing!!!
+lo punya otak gak sih?
+kalo punya otak di pakai bangsat!!
+gada otak member anjing.`
+}
